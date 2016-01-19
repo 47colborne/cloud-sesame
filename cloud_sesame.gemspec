d06fa7098317fc55aebb7602b0c38b19e52f2c6f
scottchu <scott@retailcommon.com>
2016-01-19 15:26:49 -0500

    [sc al] add default value to field

--- cloud_sesame.gemspec
+++ cloud_sesame.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'CloudSesame'
-  s.version     = '0.2.4'
+  s.version     = '0.2.5'
   s.date        = '2016-01-14'
   s.summary     = "AWS CloudSearch Query DSL"
   s.description = "AWS CloudSearch Query DSL"
--- lib/cloud_sesame/query/node/filter_query.rb
+++ lib/cloud_sesame/query/node/filter_query.rb
@@ -8,7 +8,20 @@ module CloudSesame
         end
 
         def root
-          @root ||= CloudSesame::Query::AST::Root.new context
+          @root ||= create_root_with_default_values
+        end
+
+        private
+
+        def create_root_with_default_values
+          root = CloudSesame::Query::AST::Root.new context
+          context[:fields].each do |field, options|
+            if options && (block = options[:default])
+              value = root.instance_exec &block
+              root << AST::Literal.new(field, value, options)
+            end
+          end if context[:fields]
+          root
         end
 
       end
