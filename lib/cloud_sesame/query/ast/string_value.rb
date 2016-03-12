8ecae9ff806c2a9e9bb78aae6d47ac2acae6bd80
scottchu <scottchu@icloud.com>
2016-03-12 02:41:34 -0500

    Replace regex in StringValue with compiled regex

--- lib/cloud_sesame/query/ast/string_value.rb
+++ lib/cloud_sesame/query/ast/string_value.rb
@@ -3,8 +3,8 @@ module CloudSesame
 		module AST
 			class StringValue < Abstract::Value
 
-				SINGLE_QUATE = /\'/
-				ESCAPE_QUATE = "\\'"
+				SINGLE_QUATE = Regexp.new(/\'/)
+        ESCAPE_QUATE = Regexp.new("\\'")
 
 				def self.parse(value)
 					new(value.is_a?(String) ? value : value.to_s) if value
