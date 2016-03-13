cc5be11c4ebd48bb05384bfaa105983e3a1e06d9
scottchu <scottchu@icloud.com>
2016-03-13 17:28:03 -0400

    convert tabs to whitespace

--- spec/cloud_sesame/query/builder_spec.rb
+++ spec/cloud_sesame/query/builder_spec.rb
@@ -4,14 +4,14 @@ module CloudSesame
   module Query
     describe Builder do
 
-    	it_behaves_like 'FieldAccessors' do
-				subject { Builder.new({}, "Searchable") }
+      it_behaves_like 'FieldAccessors' do
+        subject { Builder.new({}, "Searchable") }
         before {
             Builder.send(:include, DSL::FieldAccessors)
             Domain::Block.send(:include, DSL::FieldAccessors)
-        	allow(_scope).to receive(:context).and_return(context)
+          allow(_scope).to receive(:context).and_return(context)
         }
-    	end
+      end
 
     end
   end
