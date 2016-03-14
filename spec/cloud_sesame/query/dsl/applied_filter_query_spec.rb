4ac575ac4aeb87436bded70d4d326436038f3eae
scottchu <domaru.tdot@gmail.com>
2016-03-14 10:22:15 -0400

    fix AppliedFilterQuery#applied_filters, where bang! methods returns nil when no change is made.

---
+++ spec/cloud_sesame/query/dsl/applied_filter_query_spec.rb
@@ -0,0 +1,20 @@
+shared_examples AppliedFilterQuery do
+
+  describe '#applied_filters' do
+
+  end
+
+  describe '#included?' do
+
+  end
+
+  describe '#excluded?' do
+
+  end
+
+  describe '#applied?' do
+
+  end
+
+
+end
